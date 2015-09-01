6c266fc2abba6db8a6e50cfd17c921a8b34eab3d
Christian Ege <k4230r6@gmail.com>
2015-09-01 22:45:17 +0200

    Enable both UARTs
    For testing I added some debug mnessages
    
    Signed-off-by: Christian Ege <k4230r6@gmail.com>

--- src/app/blinky/src/blinky.c
+++ src/app/blinky/src/blinky.c
@@ -24,8 +24,26 @@
  *
  */
 
-#include "user_config.h" /* This is needed by the SDK */
+/* user configuration from driver_lib */
+#include "user_config.h"
 
+/* driver_lib includes */
+#include "driver/uart.h"
+
+/* SDK includes */
+#include "osapi.h"
+#include "ets_sys.h"
+#include "gpio.h"
+#include "os_type.h"
+
+/* ssystem includes */
+#include <stdint.h>
+
+ /******************************************
+  * external variables
+  ******************************************/
+
+extern UartDevice UartDev; /* defined in the SDK code */
 /******************************************
  * static variables
  ******************************************/
@@ -51,6 +69,14 @@ void ICACHE_FLASH_ATTR user_init() {
     /* set PIN2 to high */
     GPIO_OUTPUT_SET(GPIO_ID_PIN(5),1);
 
+    /* uart init */
+    /* UartDev is defined in SDK code */
+    UartDev.data_bits = EIGHT_BITS;
+    UartDev.parity = NONE_BITS;
+    UartDev.stop_bits = ONE_STOP_BIT;
+    UartDev.baut_rate = BIT_RATE_115200;
+    uart_init(BIT_RATE_115200, BIT_RATE_115200);
+
     /* clear any running timer */
     os_timer_disarm(&blinkTimer /* timer instance */);
 
@@ -59,8 +85,10 @@ void ICACHE_FLASH_ATTR user_init() {
                    (os_timer_func_t *) doBlink, /* function to be called on expiration */
                                           NULL  /* context pointer for the timer function */ );
     os_timer_arm( &blinkTimer, /* timer instance */
-                          500, /* period in ms */
+                          1000, /* period in ms */
                             1  /* 0 == singleshot, 1 == continuous*/  );
+    ets_printf("Starting blinky demo application \n\r");
+    system_set_os_print(0);
 
 }
 
@@ -68,8 +96,9 @@ void ICACHE_FLASH_ATTR user_init() {
  *
  * @param arg here we can provide some context to the timer
  */
-void doBlink(void* __attribute__((unused)) arg /* context */  )
+void doBlink(void* arg __attribute__((unused))  /* context */  )
 {
     uint32_t nextState = GPIO_INPUT_GET(GPIO_ID_PIN(5));
     GPIO_OUTPUT_SET(GPIO_ID_PIN(5), 0x00000001 ^ nextState); /* toggle the bit */
+    ets_printf("Blinky demo application \n\r");
 }
